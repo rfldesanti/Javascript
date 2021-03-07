20eeb294bdb98ea92caef84e0fc097e85bbb9844
rfldesanti <rafaelsr2018@gmail.com>
2021-03-07 13:53:04 -0300

    Anoter way to declare variables with 'let' statement

--- Scope/Scope.js
+++ Scope/Scope.js
@@ -14,3 +14,10 @@ console.log("Here is the main body of the js document, where the value of x is "
 console.log(x);
 console.log(" because it was defined as " + x + " by 'let' statement at the beggining of the document");
 //expected output: 1
+
+//You can declare a list of variables in the same statement
+
+let n1 = 1, n2 = 2, n3 = 3;
+
+console.log("A list of variables");
+console.log(n1 + " " + n2 + " " + n3);
