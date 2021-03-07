c4c3de1e80b103db608671948ca9436d265fa500
rfldesanti <rafaelsr2018@gmail.com>
2021-03-07 15:06:30 -0300

    Put some comments about the code 'Lexical Scoping'

--- Lexical_Scoping/Scoping.js
+++ Lexical_Scoping/Scoping.js
@@ -1,7 +1,15 @@
+//Lexical scoping
+
+//The code bellow shows that a inner function - a function inside other function -
+//can utilize the variables declared on it's parent function (the function 'above' it)
+//Note that displayName() does not have any variable, but utilizes
+//the variable declared on init() function
+//This shows that a inner function have access to the variables of outer functions
+
 function init() {
-    var name = 'Mozilla';
-    function displayName() {
-        alert(name);
+    var name = 'Mozilla'; //declare a variable called 'name' with 'Mozilla' as value
+    function displayName() { //creates an inner functions
+        alert(name);//that utilizes the 'name' variable declared on it's parent function
     }
     displayName();
 }
