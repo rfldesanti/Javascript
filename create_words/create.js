7b939c4fed9718d20e45152de151ed42fc75ed60
rfldesanti <rafaelsr2018@gmail.com>
2021-03-15 23:26:32 -0300

    Testing a code that create words

---
+++ create_words/create.js
@@ -0,0 +1,22 @@
+function create(len) {
+
+    let word = '';
+    const vowels = 'aeiou';
+    const consonants = 'bcdfghjklmnpqrstvwxyz';
+    const random = n => Math.floor(Math.random() * n) + 1;
+
+    if(!len) len = random(10);
+    console.log(len);
+    if(len < 3) len = 3;
+    console.log(len);
+
+    for (let i=0; i<len; i++) {
+        word += i % 2 === 0 ?
+            consonants[random(consonants.length) - 1] : vowels[random(vowels.length) - 1];
+    }
+
+    return word;
+
+}
+
+console.log(create());
