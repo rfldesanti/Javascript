7d9da7edc259cfbd970862bfdc5bc5b3cdf9473a
rfldesanti <rafaelsr2018@gmail.com>
2021-03-20 13:46:11 -0300

    Put some explanatinos on code

--- create_words/create.js
+++ create_words/create.js
@@ -1,10 +1,13 @@
 function create(len) {
-
+//Creates da function called "create" that receives a parameter called "len"
     let word = '';
+    //creates a string variable called "word" with empty value
     const vowels = 'aeiou';
+    //declares a const string variable called "vowels" with the 'aeiou' value
     const consonants = 'bcdfghjklmnpqrstvwxyz';
+    //declares a const variable called consonants and put all the alphabetical consonants
     const random = n => Math.floor(Math.random() * n) + 1;
-
+    //declares a const variable called random and gives to it 
     if(!len) len = random(10);
     console.log(len);
     if(len < 3) len = 3;
