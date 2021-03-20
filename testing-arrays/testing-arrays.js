60714d6a130dd8a3e9d7b3b64dfe69ed0b8a62c6
rfldesanti <rafaelsr2018@gmail.com>
2021-03-20 15:10:48 -0300

    Making tests on arrow functions, function expression and arrays

--- testing-arrays/testing-arrays.js
+++ testing-arrays/testing-arrays.js
@@ -1,4 +1,37 @@
-const guitars_brands = ['fender', 'gibson', 'SG', 'Epiphone', 'tagima'];
+const guitars_brands = [
+    'fender',
+    'gibson',
+    'SG',
+    'Epiphone',
+    'tagima'
+];
+
+console.log('Array guitars_brands: ' + guitars_brands);
+
+var brands_size = guitars_brands.map(function(guitar_brand) {
+    return guitar_brand.length;
+})
+
+console.log('Array brands_size with function expression and declaring new var: ' + brands_size);
+
+console.log('Array guitars_brands with function expression without another var: ' + guitars_brands.map(function(guitar_brand2) {
+    return guitar_brand2.length;
+}))
+
+const guitars_brands2 = [
+    'PRS',
+    'Dean',
+    'Takamine',
+    'Ibanez',
+    'Washburn',
+    'Yamaha',
+    'Guild',
+];
+
+console.log('guitars_brands2 array: ' + guitars_brands2);
+var guitars_brands2_size = guitars_brands2.map(guitars2 => guitars2.length);
+console.log('guitars_brands2_size with arrow function and declaring new var: ' + guitars_brands2_size);
+console.log('guitars_brands2 size of elements without declaring another var: ' + guitars_brands2.map(guitars3 => guitars3.length));
 
 //a little example taked from de MDN site
 const materials = [
@@ -8,7 +41,8 @@ const materials = [
     'Beryllium'
 ];
 
-console.log(materials.map(material => material.length));
+console.log('Materials array: ' + materials);
+console.log('Materials elements size with arrow function: ' + materials.map(material => material.length));
 //end of example
 
 //the same example above, but without the arrow function
@@ -27,6 +61,7 @@ var materials2_size = materials2.map(function(materials) {
     return materials.length;
 })
 
-console.log(materials2_size);
+console.log('Materials2 array: ' + materials2);
+console.log('Materials size array with no arrow function: ' + materials2_size);
 
 //end of example
