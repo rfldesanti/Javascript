d2b89c4fb275663ac07857f2a01b937dc1eb6408
rfldesanti <rafaelsr2018@gmail.com>
2021-03-20 16:16:25 -0300

    Testing a lot the array functions!

--- testing-arrays/testing-arrays.js
+++ testing-arrays/testing-arrays.js
@@ -1,6 +1,6 @@
 
 //with the 'const' statement, it's possible to maintain the value of the variable IMMUTABLE
-//but, in the case of an array, you can change the values inside of it
+//but, in the case of an array, you can change the values inside of it, but not the entire array
 const guitars_brands = [
     'fender',
     'gibson',
@@ -9,13 +9,14 @@ const guitars_brands = [
     'tagima'
 ];
 
+//show the array guitars_brands
 console.log('Array guitars_brands: ' + guitars_brands);
 
 //creating a function to work in array with functiom expression and declaring a new var
 var brands_size = guitars_brands.map(function(guitar_brand) {
     return guitar_brand.length;
 })
-
+//display the var array brands_size
 console.log('Array brands_size with function expression and declaring new var: ' + brands_size);
 
 //expressing the .map method on console.log with function expression without declaring a new var
@@ -30,6 +31,19 @@ console.log('Arrow function to get the size of the elements declaring a new var:
 //creating a function to work in array with an arrow function without declaring a new var
 console.log('Arrow function to get the size of elements without declaring new var: ' + guitars_brands.map(gt_size => gt_size.length));
 
+//creating a var that receives the returned value from a function expression
+//that put a message at the side of every value of the guitars_brands array
+var gt_brands_size_fe = guitars_brands.map(function(gt_fe) {
+    return gt_fe + ' it is a very good guitar! ';
+})
+
+//display the var gt_brands_size_fe
+console.log('Display the var of the function expression that puts a message on array: ' + gt_brands_size_fe);
+
+//creating a arrow function inside console.log that return every value of the array with
+//a message
+
+console.log('Arrow function putting a value on everey guitars_brands array: ' + guitars_brands.map(gt_a => gt_a + ' it is a very good guitar! '));
 
 const guitars_brands2 = [
     'PRS',
