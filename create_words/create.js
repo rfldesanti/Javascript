9eb53b6c7ff371877248874bcf7220b35c978a86
rfldesanti <rafaelsr2018@gmail.com>
2021-03-20 21:45:32 -0300

    Putting explanations abot the code

--- create_words/create.js
+++ create_words/create.js
@@ -7,11 +7,25 @@ function create(len) {
     const consonants = 'bcdfghjklmnpqrstvwxyz';
     //declares a const variable called consonants and put all the alphabetical consonants
     const random = n => Math.floor(Math.random() * n) + 1;
-    //declares a const variable called random and gives to it 
+    //declares a const variable called random and gives to it the value of a function
+    //in other words: the random const becomes A FUNCTION
+    //the function returns the value of n parameter calculated by some
+    //mathamtical function
+    //Actually, the code does the same thing that the tree lines below
+    //const random = function(n) {
+    //    return Math.floor(Math.random() * n) + 1;
+    //}
     if(!len) len = random(10);
+    //an if conditional that says 'if variable len is empty, then the value of len
+    //it will be the value returned by the random function with an argument equals to 10'
     console.log(len);
+    //showing the value of 'len'
     if(len < 3) len = 3;
+    //an if statement that says 'if the value of the parameter len is minor
+    //than 3, makes the value of len equals to 3'
+    //this in conditional makes that len nevere receives a value minor that 3
     console.log(len);
+    //showing the value of len
 
     for (let i=0; i<len; i++) {
         word += i % 2 === 0 ?
