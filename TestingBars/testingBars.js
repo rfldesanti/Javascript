d80974c437c7ce1f301fdc99dbddd1997263382a
rfldesanti <rafaelsr2018@gmail.com>
2021-03-29 23:00:48 -0300

    Testing bars

--- TestingBars/testingBars.js
+++ TestingBars/testingBars.js
@@ -41,8 +41,40 @@ function addBlocks() {
         block.style.left = blocks[i].bottomLeft[0] + "px"
         block.style.bottom = blocks[i].bottomLeft[1] + "px"
         grid.appendChild(block)
-
     }
 }
 
 addBlocks()
+
+const bar = document.createElement('div')
+bar.classList.add('bar')
+bar.style.left = getMinorPosition() + "px"
+bar.style.bottom = 1 + "px"
+bar.style.height = 20 + "px"
+bar.style.width = getBiggerPosition() + blockWidth + "px"
+grid.appendChild(bar)
+
+console.log(getBiggerPosition())
+
+function getBiggerPosition () {
+    let position = 0;
+    for (let i = 0; i < blocks.length; i++) {
+        position = blocks[0].bottomLeft[0]
+        if (position < blocks[i].bottomLeft[0]) {
+            position = blocks[i].bottomLeft[0]
+        }
+    }
+    return position - 10
+}
+
+function getMinorPosition () {
+    let position = blocks[0].bottomLeft[0]
+    for (let i = 0; i < blocks.length; i++) {
+        if (position > blocks[i].bottomLeft[0]) {
+            position = blocks[i].bottomLeft[0]
+        }
+    }
+    return position
+}
+
+console.log(getMinorPosition())
