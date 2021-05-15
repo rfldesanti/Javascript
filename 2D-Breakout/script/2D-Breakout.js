92478eb2b4fb5be130a38d0d11fdb5bbea7ea099
rfldesanti <rafaelsr2018@gmail.com>
2021-05-15 16:46:41 -0300

    Adding moves of the user

--- 2D-Breakout/script/2D-Breakout.js
+++ 2D-Breakout/script/2D-Breakout.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
+const boardWidth = 560
 
 const userStart = [230, 10]
 let currentPosition = userStart
@@ -63,8 +64,18 @@ function drawUser() {
 function moveUser(e) {
     switch(e.key) {
         case 'ArrowLeft':
-            currentPosition[0] -=10
-            drawUser()
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10
+                drawUser()
+            }
+            break;
+        case 'ArrowRight'    :
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 10
+                drawUser()
+            }
             break;
     }
 }
+
+document.addEventListener('keydown', moveUser)
